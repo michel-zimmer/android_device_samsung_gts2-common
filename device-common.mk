e820103b1b355552e5c86e4307bd81fc9fdd622a
ripee <ripee.dev@gmail.com>
2021-01-09 00:02:32 +0100

    gts2-common: RR-ify

--- device-common.mk
+++ device-common.mk
@@ -189,7 +189,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     device/samsung/gts2-common/overlay \
-    device/samsung/gts2-common/overlay-lineage
+    device/samsung/gts2-common/overlay-rr
 
 # Permissions
 PRODUCT_COPY_FILES += \
